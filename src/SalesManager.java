6f55092343854b6c7bd549ee697c6d23c64e6337
SerjiK7373 <serjik7373@yandex.ru>
2023-01-14 23:57:57 +0400

    adding min function and average function

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,45 @@
 public class SalesManager {
-    protected int[] sales;
+    protected long[] sales;
 
-    public SalesManager(int[] sales) {
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
+    }
+
+    public long min() {
+        long min = max();
+        for (long sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public long stat() {
+        int stat = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for (long sale : sales) {
+            if (min() == sale && b == 0) {
+                b++;
+            } else if (max() == sale && c == 0) {
+                c++;
+            } else {
+                stat += sale;
+                a++;
+            }
+        }
+        return (stat / a);
     }
 }
